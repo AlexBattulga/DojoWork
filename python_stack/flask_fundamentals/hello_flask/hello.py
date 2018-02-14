1a56ad2884c1f545ba76a0fe4ba76f4e50f6acd0
Enkbaatar Battulga <ebar1126@gmail.com>
2018-02-13 19:28:08 -0500

    Updated my portfolio basic

--- python_stack/flask_fundamentals/hello_flask/hello.py
+++ python_stack/flask_fundamentals/hello_flask/hello.py
@@ -3,12 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 
-def hello_world():
+def welcome():
     return render_template('index.html')
 
-@app.route('/success')
+@app.route('/projects')
 
-def success():
-    return render_template('success.html')
-    
+def projects():
+    return render_template('projects.html')
+
+@app.route('/about')
+
+def about():
+    return render_template('about.html')
+
 app.run(debug=True)
