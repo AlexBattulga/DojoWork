763375b90ce0ed5b89755d82a55091b30f125460
Enkbaatar Battulga <ebar1126@gmail.com>
2018-03-08 19:23:51 -0500

    Python OOP Animal assignment

---
+++ python_stack/python_OOP/Animal.py
@@ -0,0 +1,43 @@
+class Animal(object):
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def walk(self):
+        self.health -= 1
+        return self
+
+    def run(self):
+        self.health -= 5
+        return self
+
+    def displayHealth(self):
+        print "{}'s health: {}".format(self.name, self.health)
+        return self
+class Dog(Animal):
+    def __init__(self, name, health = 150):
+        super(Dog, self).__init__(name, health)
+
+    def pet(self):
+        self.health += 5
+        return self
+class Dragon(Animal):
+    def __init__(self, name, health = 170):
+        super(Dragon, self).__init__(name, health)
+
+    def fly(self):
+        self.health -= 10
+        return self.health
+
+    def displayHealth(self):
+        print 'I am a DRaGON'
+        super(Dragon, self).displayHealth()
+
+dog = Animal('Totoro', 100)
+dog.walk().walk().walk().run().run().displayHealth()
+
+small_dog = Dog('Johnny')
+small_dog.walk().walk().walk().run().run().displayHealth()
+
+dragon = Dragon('Drogon')
+dragon.walk().walk().displayHealth()
